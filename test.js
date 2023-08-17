cc40ec7f998bad9aad1ba82984110761419ad2ed
Hexer <hex@hust.edu.cn>
2023-08-17 23:24:33 +0800

    新添加headers.h

--- test.js
+++ test.js
@@ -2,11 +2,11 @@
  * @Author: Hex
  * @Date: 2023-08-17 21:31:50
  * @LastEditors: Hexer hex@hust.edu.cn
- * @LastEditTime: 2023-08-17 22:22:50
+ * @LastEditTime: 2023-08-17 23:24:03
  * @FilePath: \gitlearning\test.js
  * @Description: 
  */
 {
-    {this is a dead file},
+    some can be delete
 
 }
