bcc524699275f3c4a3c3795c85c5d6866ec0df63
Hexer <hex@hust.edu.cn>
2023-08-17 22:27:19 +0800

    修改了test文件

--- test.js
+++ test.js
@@ -2,11 +2,11 @@
  * @Author: Hex
  * @Date: 2023-08-17 21:31:50
  * @LastEditors: Hexer hex@hust.edu.cn
- * @LastEditTime: 2023-08-17 21:50:40
+ * @LastEditTime: 2023-08-17 22:22:50
  * @FilePath: \gitlearning\test.js
  * @Description: 
  */
 {
-    
+    {this is a dead file},
 
 }
