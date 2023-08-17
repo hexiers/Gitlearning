518b2d1145189ab2a2dbe98ea91548aa4e9af1ab
Hexer <hex@hust.edu.cn>
2023-08-18 00:39:45 +0800

    删除两个测试文件

--- headers.h
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: Hex
- * @Date: 2023-08-17 23:21:33
- * @LastEditors: Hexer hex@hust.edu.cn
- * @LastEditTime: 2023-08-17 23:22:36
- * @FilePath: \gitlearning\headers.h
- * @Description: 
- */
-#include "test.js"
